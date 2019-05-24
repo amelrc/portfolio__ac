f263cef8c5aa25fa3121078a323cb14f4d562bce
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2019-05-24 15:10:52 +0200

    slider on scroll

--- js/about.js
+++ js/about.js
@@ -1,23 +1,24 @@
 
 let cross = document.querySelector('.cross');
+let myPicture = document.querySelector('.picture');
 
 setTimeout(function(){
   let header = document.querySelector('.about__header');
   let subHeader = document.querySelector('.about__sub-header');
   let content = document.querySelector('.about__content');
   
-  header.style.marginTop = '140px';
+  header.style.marginTop = '70px';
   header.style.visibility = 'visible';
 
-  subHeader.style.marginTop = '0';
+  subHeader.style.marginTop = '-30px';
   subHeader.style.visibility = 'visible';
 
-  content.style.marginTop = '-170px';
+  content.style.marginTop = '-150px';
   content.style.visibility = 'visible';
 }, 0);
 
 setTimeout(function(){
-  let myPicture = document.querySelector('.picture');
+  
   myPicture.style.marginBottom = '10px';
   myPicture.style.visibility = 'visible';
 }, 0)
@@ -28,11 +29,23 @@ setTimeout(function(){
   aboutBig.style.opacity = '0.06';
 }, 0);
 
-document.querySelector('.container__body').addEventListener("scroll", myFunction);
+/////////////////////
 
-function myFunction() {
-  
-  if (document.body.scrollTop > 3 || document.documentElement.scrollTop > 3) {
-    console.log('hello')
-  }
-}
+const full = document.querySelector('.wrapper__left-side--full')
+
+full.addEventListener('wheel', (e) => {
+
+    const savedNum = e.deltaY;
+    console.log(e.deltaY);
+    console.log(savedNum);
+
+    const functionA = () => {
+     console.log('this is function A')
+    }
+
+    const functionB = () => {
+      console.log("functionB")
+    }
+    savedNum > 0 ? functionA() : functionB()
+
+  });
--- js/main.js
+++ js/main.js
@@ -17,9 +17,41 @@ function arrowClick() {
     }
     setTimeout(function(){
         window.location.replace('./about.html')
-    }, 1000); 
+    }, 800); 
 }
 
+///////////////////////
+
+const arr = ['../img/p3.jpg','../img/cat.png','../img/moon.png', '../img/butterflies.jpg', '../img/me.png'];
+let index = 0;
+const container = document.querySelector('.container__body');
+let savedNum;
+const images = document.querySelector('#images');
+
+const up = () => {
+    console.log(arr[index])
+    images.setAttribute('src', arr[index--])
+    
+    if(index < 0){
+        console.log("what is this")
+        index = 4
+    }
+}
+    
+const down = () => {
+    console.log(arr[index])
+  
+    
+    images.setAttribute('src', arr[index++])
+    if(index > 4){index = 0}
+      
+}
+
+container.addEventListener('wheel', (e) => {
+    console.log(savedNum, "hereeeeee")
+      savedNum = e.deltaY;
+      savedNum < 0 ? up() : down()
+});
 
 
 
