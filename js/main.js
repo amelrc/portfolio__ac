eeef4529618339564a846e24abed6cac7771c602
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2019-06-14 16:06:56 +0200

    mobile version works

--- js/main.js
+++ js/main.js
@@ -1,60 +1,31 @@
 
-let arrow = document.querySelector('.arrow__image');
-let leftSide = document.querySelector('.wrapper__left-side');
-let transition = document.querySelectorAll('.transition-left');
-let projects = document.querySelector('.wrapper__projects');
+let bg1 = document.querySelector('.container__body');
+let headerBody = document.querySelector('.header-body');
+let projects = document.querySelectorAll('.project');
+let projectLink = document.querySelectorAll('.p-link');
+const modifiers = ['background--1','background--2','background--3','background--4','background--5']
 
-arrow.addEventListener('click', arrowClick);
 
-function arrowClick() {
-    arrow.style.display = 'none';
-    leftSide.style.width = '100%';
-    leftSide.style.backgroundColor = '#32820A';
-    projects.classList.remove('wrapper__projects')
-    for (let i = 0; i < transition.length; i++) {
-        transition[i].style.left = '334px';
-    }
-    setTimeout(function(){
-        window.location.replace('./about.html')
-    }, 800); 
-}
-
-// Image Gallery //
-
-const arr = ['../img/p3.jpg','../img/cat.png','../img/moon.png', '../img/butterflies.jpg', '../img/me.png'];
-let index = 0;
-const container = document.querySelector('.container__body');
-let savedNum;
-const images = document.querySelector('#images');
-
-const up = () => {
-    console.log(arr[index], 'up movement')
-    
-    images.setAttribute('src', arr[index--])
-    if(index < 0 ){
-        index = 4
-    }
-}
-    
-const down = () => {
-    console.log('down movement', arr[index] )
-    
-    images.setAttribute('src', arr[index++])
-
-    console.log(arr[index++])
-    if(index > 4) {
-        index = 0
-    }
-}
-
-container.addEventListener('wheel', (e) => {
-    console.log(savedNum, "delta Y number")
-      savedNum = e.deltaY;
-      savedNum < 0 ? up() : down()
+projects.forEach(project => {
+    project.addEventListener('click', (el) => navClick(el))
 });
 
+function navClick(el) {
+    const showLink = el.path[0].children[0];
+    
+     const test = el.path[0]
 
+    headerBody.classList.add('hidden');
+    showLink.classList.remove('hidden');
+    
+    modifiers.forEach(bgClass => {
 
+        const projectNumber = el.path[0].classList[1].charAt(9);
+        const classNumber = bgClass.charAt(12);
 
+        (projectNumber === classNumber) ? bg1.classList.add(bgClass) : bg1.classList.remove(bgClass);
+        
+    });
+}
 
 
