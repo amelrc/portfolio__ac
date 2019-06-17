1026234c82eb18a72444bf2541c5da705b6bd624
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2019-06-17 16:45:24 +0200

    home page fixed in both

--- js/main.js
+++ js/main.js
@@ -10,10 +10,30 @@ projects.forEach(project => {
     project.addEventListener('click', (el) => navClick(el))
 });
 
+projects.forEach(project => {
+    project.addEventListener('mouseover', (el) => navHover(el))
+});
+
+function navHover(el) {
+    const showLink = el.path[0].children[0];
+    console.log(showLink);
+   
+    headerBody.classList.add('hidden');
+    showLink.classList.remove('hidden');
+
+    modifiers.forEach(bgClass => {
+        const projectNumber = el.path[0].classList[1].charAt(9);
+        const classNumber = bgClass.charAt(12);
+        
+        console.log('entered the zone', projectNumber);
+        
+        (projectNumber === classNumber) ? bg1.classList.add(bgClass) : bg1.classList.remove(bgClass);
+        
+    });
+}
+
 function navClick(el) {
     const showLink = el.path[0].children[0];
-    
-     const test = el.path[0]
 
     headerBody.classList.add('hidden');
     showLink.classList.remove('hidden');
