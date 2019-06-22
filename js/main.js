2cb56209c7faf2fe07e28f20b49c7d69d46b258d
Amel Ramirez <amelramirez@Amels-Air.dynamic.ziggo.nl>
2019-06-22 21:28:28 +0200

    menu worksin home page

--- js/main.js
+++ js/main.js
@@ -3,16 +3,54 @@ let bg1 = document.querySelector('.container__body');
 let headerBody = document.querySelector('.header-body');
 let projects = document.querySelectorAll('.project');
 let projectLink = document.querySelectorAll('.p-link');
-const menu = document.querySelector('.menu');
+let wrapperProjects = document.querySelector('.wrapper__projects')
 
-const modifiers = ['background--1','background--2','background--3','background--4','background--5'];
+const crossMenu = document.querySelector('.cross--menu');
+const menu = document.querySelector('.menu');
+const modifiers = [
+    'background--1',
+    'background--2',
+    'background--3',
+    'background--4',
+    'background--5'
+];
+
+menu.addEventListener('click', openMenu);
+crossMenu.addEventListener('click', closeMenu);
+
+function openMenu() {
+
+    let openMenu = document.querySelector('.open-menu');
+
+    openMenu.classList.remove('hidden-test');
+    headerBody.classList.add('hidden-test');
+    wrapperProjects.classList.add('hidden-test');
+}
+
+function closeMenu() {
+
+    let openMenu = document.querySelector('.open-menu');
+
+    openMenu.classList.add('hidden-test');
+    headerBody.classList.remove('hidden-test');
+    wrapperProjects.classList.remove('hidden-test');
+
+}
+
+
+
+
+
+
+
+
+
 
 
 projects.forEach(project => {
     project.addEventListener('click', (el) => navClick(el))
 });
 
-var p = null;
 projects.forEach(project => {
     project.addEventListener('mouseenter', (el) => navHover(el))
 });
