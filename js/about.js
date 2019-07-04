ce8b500727105b5f5377fc97bf26daddb41f8245
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2019-07-04 15:54:28 +0200

    js cleaning

--- js/about.js
+++ js/about.js
@@ -22,39 +22,3 @@ function closeMenu() {
     openMenu.classList.add('closed-menu');
     wrapper.style.display = 'flex';
 }
-
-
-
-
-
-
-
-
-
-
-setTimeout(function(){
-  let header = document.querySelector('.about__header');
-  let subHeader = document.querySelector('.about__sub-header');
-  let content = document.querySelector('.about__content');
-  
-  // header.style.marginTop = '70px';
-  // header.style.visibility = 'visible';
-
-  // subHeader.style.marginTop = '-30px';
-  // subHeader.style.visibility = 'visible';
-
-  // content.style.marginTop = '-150px';
-  // content.style.visibility = 'visible';
-}, 0);
-
-setTimeout(function(){
-  
-  // myPicture.style.marginBottom = '10px';
-  myPicture.style.visibility = 'visible';
-}, 0)
-
-setTimeout(function(){ 
-  let aboutBig = document.querySelector('.bg-about');
-  aboutBig.classList.remove('hidden');
-  aboutBig.style.opacity = '0.06';
-}, 0);
