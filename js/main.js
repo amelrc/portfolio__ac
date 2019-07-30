11cc5b9b2bb6e6f419ab99d0f384b6d15b3ad517
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2019-07-30 14:11:21 +0200

    animation nav

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ let projects = document.querySelectorAll('.project');
 let wrapperProjects = document.querySelector('.wrapper__projects');
 
 menu.addEventListener('click', openMenu);
-// crossMenu.addEventListener('click', closeMenu);
 
 function openMenu() {
     let openMenu = document.querySelector('.open-menu');
@@ -25,68 +24,50 @@ function openMenu() {
     wrapperProjects.classList.add('closed-menu');
 }
 
-// function closeMenu() {
-//     let openMenu = document.querySelector('.open-menu');
-//     openMenu.classList.add('closed-menu');
-//     headerBody.classList.remove('closed-menu');
-//     wrapperProjects.classList.remove('closed-menu');
-// }
-
 // Projects Nav
 
 projects.forEach(project => {
     project.addEventListener('click', (el) => navClick(el))
 });
 
-projects.forEach(project => {
-    project.addEventListener('mouseover', (el) => navHover(el))
-});
+if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    projects.forEach(project => {
+        project.addEventListener('mouseover', (el) => navHover(el))
+    });
 
-projects.forEach(project => {
-    project.addEventListener('mouseout', (el) => navOutHover(el))
-});
-
+    projects.forEach(project => {
+        project.addEventListener('mouseout', (el) => navOutHover(el))
+    });
+}
+    
 function navOutHover(el) {
-
     headerBody.classList.remove('hidden');
-
     modifiers.forEach(bgClass => {
-
         const projectNumber = el.currentTarget.classList[1].charAt(9);
         const classNumber = bgClass.charAt(12);
-        
         (projectNumber === classNumber) ? bg1.classList.remove(bgClass) : bg1.classList.add('container__body');
-        
     });
 }
 
 function navHover(el) {
-
     const showLink = el.currentTarget.children[0];
     headerBody.classList.add('hidden');
     setTimeout(function() {
-        
         showLink.classList.remove('hidden');
-
     }, 10);
-
     modifiers.forEach(bgClass => {
-
         const projectNumber = el.currentTarget.classList[1].charAt(9);
         const classNumber = bgClass.charAt(12);
-        
         (projectNumber === classNumber) ? bg1.classList.add(bgClass) : bg1.classList.remove(bgClass);
-        
     });
 }
 
 function navClick(el) {
     headerBody.classList.add('hidden');
-   
     projects.forEach(project => {
         if (project === el.currentTarget) {
             project.children[0].classList.remove('hidden');
-        } else if (project.children.length > 0) {
+        } else {
             project.children[0].classList.add('hidden');
         }
     })
@@ -100,24 +81,10 @@ function navClick(el) {
     }); 
 }
 
-function hideLink() {
-    projects.forEach(project => {
-        if(project.children.length > 0) {
-            if(!project.children[0].classList.contains('hidden')) {
-                project.children[0].classList.add('hidden');
-            }
-        }
-    })
-}
-
 bg1.addEventListener('click', (e) => {
-
     let projectBG = bg1.classList[1];
-    hideLink()
-
+   
     if (e.target === e.currentTarget || e.target === bg1.children[2]) {
-        
-        hideLink()
         bg1.classList.remove(projectBG);
         headerBody.classList.remove('hidden');  
     }   
